978df7666a52b8a38416c0b0f72d29708247d593
lokiharsha <44457615+lokiharsha@users.noreply.github.com>
2024-07-08 19:46:58 +0530

    Update privider.tf

--- privider.tf
+++ privider.tf
@@ -1,6 +1,6 @@
 provider "aws"{
   region="us-east-1"
-  access_key = "AKIA2UC3E7CMK7GELTHN"
-  secret_key ="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"
+  access_key = "AKIA2UC276YV2JYFLTPZ"
+  secret_key ="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"
 
 }
